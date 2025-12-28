dc537b48f67335bc616954c7557e46d086ee5056
Seiya Sakaguchi <s4e2i4ya@gmail.com>
2025-12-28 10:33:48 +0900

    chore: WPFプロジェクトの初期構成を追加

---
+++ ConsoleHttpClient/Program.cs
@@ -0,0 +1,19 @@
+﻿using HttpClientLibraty;
+
+public class Programa
+{
+    static void Main(string[] args)
+    {
+		try
+		{
+			var client = Client.Instance;
+			client.Initialize();
+			var message = client.GetMessage(string.Empty);
+		}
+		catch (Exception  ex)
+		{
+            Console.WriteLine(ex.Message);
+		}
+    }
+}
+
