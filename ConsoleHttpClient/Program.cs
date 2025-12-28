1036f54f376a7e91291139fa81b5acf41bc71376
Seiya Sakaguchi <s4e2i4ya@gmail.com>
2025-12-28 12:02:59 +0900

    first commit

--- ConsoleHttpClient/Program.cs
+++ ConsoleHttpClient/Program.cs
@@ -1,8 +1,19 @@
-﻿public class Program
+﻿using HttpClientLibraty;
+
+public class Programa
 {
     static void Main(string[] args)
     {
-        
+		try
+		{
+			var client = Client.Instance;
+			client.Initialize();
+			var message = client.GetMessage(string.Empty);
+		}
+		catch (Exception  ex)
+		{
+            Console.WriteLine(ex.Message);
+		}
     }
 }
 
---
+++ HttpClientLibraty/Client.cs
@@ -0,0 +1,56 @@
+﻿using Newtonsoft.Json;
+
+namespace HttpClientLibraty
+{
+    public class Client
+    {
+        /// <summary>
+        /// シングルトン
+        /// </summary>
+        private static Client? _instance;
+        public static Client Instance => _instance ??= new Client();
+
+        /// <summary>
+        /// HttpClientインスタンス
+        /// </summary>
+        private HttpClient _httpClient = new();
+
+        private Client()
+        {
+        }
+
+        public void Initialize()
+        {
+            var config = ConfigManager.GetConfigData();
+            var baseAddress = $"http://{config.Host}:{config.Port}/{config.Path}";
+
+            // HttpClientの初期化処理
+            _httpClient.BaseAddress = new Uri(baseAddress);
+            _httpClient.Timeout = TimeSpan.FromSeconds(config.TimeoutSeconds);
+        }
+
+        public string GetMessage(string command)
+        {
+            var httpResponseMessage = Get(command);
+            return httpResponseMessage.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+        }
+
+        private HttpResponseMessage Get(string command)
+        {
+            try
+            {
+                return _httpClient.GetAsync(command).GetAwaiter().GetResult();
+            }
+            catch (TaskCanceledException)
+            {
+                // タイムアウト
+                throw;
+            }
+            catch (Exception)
+            {
+                // その他のエラー
+                throw;
+            }
+        }
+    }
+}
