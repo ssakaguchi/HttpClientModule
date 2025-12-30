2f6babee0252e418830694dde1fbd71458325e22
Seiya Sakaguchi <s4e2i4ya@gmail.com>
2025-12-30 20:18:29 +0900

    必要以上にHttpClientのインスタンスを作り直さないよう修正

--- HttpClientLibraty/Client.cs
+++ HttpClientLibraty/Client.cs
@@ -8,14 +8,18 @@
         private static Client? _instance;
         public static Client Instance => _instance ??= new Client();
 
+        /// <summary> 設定中のベースアドレス </summary>
+        private string? _currentBaseAddress;
+
         /// <summary>
-        /// HttpClientインスタンス
+        /// 設定中のタイムアウト時間（秒）
         /// </summary>
+        private int _currentTimeoutSeconds;
+
+        /// <summary> HttpClientインスタンス </summary>
         private HttpClient _httpClient = new();
 
-        private Client()
-        {
-        }
+        private Client() { }
 
         public string GetMessage(string command)
         {
@@ -25,19 +29,16 @@
 
         private HttpResponseMessage Get(string command)
         {
+            var config = ConfigManager.GetConfigData();
+
+            // Httpクライアントの設定
+            EnsureHttpClient(config);
+
+            using var request = new HttpRequestMessage(HttpMethod.Get, command);
+
             try
             {
-                var config = ConfigManager.GetConfigData();
-                var baseAddress = $"http://{config.Host}:{config.Port}/{config.Path}";
-
-                // HttpClientの初期化処理
-                _httpClient = new HttpClient
-                {
-                    BaseAddress = new Uri(baseAddress),
-                    Timeout = TimeSpan.FromSeconds(config.TimeoutSeconds)
-                };
-
-                return _httpClient.GetAsync(command).GetAwaiter().GetResult();
+                return _httpClient.SendAsync(request).GetAwaiter().GetResult();
             }
             catch (TaskCanceledException)
             {
@@ -49,6 +50,30 @@
                 // その他のエラー
                 throw;
             }
+        }
+
+        /// <summary> Httpクライアントの設定 </summary>
+        /// <remarks>前回送信時と設定が変わっていない場合は何もしない</remarks>
+        private void EnsureHttpClient(ConfigData config)
+        {
+            var baseAddress = $"http://{config.Host}:{config.Port}/{config.Path.Trim('/')}";
+
+            var timeoutSeconds = config.TimeoutSeconds;
+            if (_currentBaseAddress == baseAddress && _currentTimeoutSeconds == timeoutSeconds)
+            {
+                return;
+            }
+
+            // HttpClientの初期化処理
+            _httpClient.Dispose();
+            _httpClient = new HttpClient
+            {
+                BaseAddress = new Uri(baseAddress),
+                Timeout = TimeSpan.FromSeconds(config.TimeoutSeconds)
+            };
+
+            _currentBaseAddress = baseAddress;
+            _currentTimeoutSeconds = timeoutSeconds;
         }
     }
 }
